704bf4315fb33b3f2ddea54ad5822eb7293f23d2
WieserJulian <julian.wieser@i-wieser.com>
2023-09-09 11:58:34 +0200

    Had and idea

--- src/main.py
+++ src/main.py
@@ -1,15 +1,19 @@
-import sys
-from tool_frames.Display import Display
-from utils.config import Config
-import logging
-if getattr(sys, 'frozen', False):
-    import pyi_splash
-
 if __name__ == '__main__':
-    Config()
-    # logging.basicConfig(filename='log/wasApp.log', encoding='utf-8', level=logging.DEBUG)
-    logging.info("Start application")
-    app = Display()
-    if getattr(sys, 'frozen', False):
-        pyi_splash.close()
-    app.mainloop()
+    try:
+        import sys
+        from tool_frames.Display import Display
+        from utils.config import Config
+        import logging
+
+        if getattr(sys, 'frozen', False):
+            import pyi_splash
+        Config()
+        # logging.basicConfig(filename='log/wasApp.log', encoding='utf-8', level=logging.DEBUG)
+        logging.info("Start application")
+        app = Display()
+        if getattr(sys, 'frozen', False):
+            pyi_splash.close()
+        app.mainloop()
+    except Exception as e:
+        print("Error", e)
+        input()
